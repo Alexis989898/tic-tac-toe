859a92dcb27548852a433b8f1626da914cb40f80
Alexis989898 <alexisvitor6@gmail.com>
2024-02-23 17:38:47 -0300

    Working console game

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let Gameboard = {
-    board: Array(9).fill("1"),
+    board: Array.from({ length: 10 }, (_, index) => index),
+    turnPlayer: "X",
     printBoard: function() {
         let boardString = "";
         for(let i = 0; i < 9; i++) {
@@ -11,9 +12,64 @@ let Gameboard = {
             }
         }
         console.log(boardString);
+    },
+    playerAction: function() {
+        let playerChoice = prompt(this.turnPlayer + "'s turn. Choose a space:");
+        while(this.board[playerChoice] == "X" || this.board[playerChoice] == "O" 
+        || playerChoice == NaN || this.board[playerChoice] == null) {
+            playerChoice = prompt("Incorrect. Choose an available space:")
+        }
+        this.board[playerChoice] = this.turnPlayer;
+    },
+    switchPlayer: function() {
+        if(this.turnPlayer == "X") {
+            this.turnPlayer = "O";
+        } else {
+            this.turnPlayer = "X";
+        }
+    },
+    winCheck: function() {
+        let win = false;
+        for(let i = 0; i < 3; i++) {
+            if(
+                (this.board[i * 3] == this.turnPlayer &&
+                this.board[i * 3 + 1] == this.turnPlayer &&
+                this.board[i * 3 + 2] == this.turnPlayer) 
+                    ||
+                (this.board[i] == this.turnPlayer &&
+                this.board[i + 3] == this.turnPlayer &&
+                this.board[i + 6] == this.turnPlayer)
+            ) {
+                this.printBoard();
+                console.log(this.turnPlayer + " won!");
+                win = true;
+                break;
+            }
+        }
+        if( 
+            (this.board[0] == this.turnPlayer &&
+            this.board[4] == this.turnPlayer &&
+            this.board[8] == this.turnPlayer)
+                ||
+            (this.board[2] == this.turnPlayer &&
+                this.board[4] == this.turnPlayer &&
+                this.board[6] == this.turnPlayer)
+            )  {
+                this.printBoard();
+                console.log(this.turnPlayer + " won!");
+                win = true;
+            }
+
+        return win;
     }
 };
 
 let round = (function() {
-    Gameboard.printBoard();
+    let win = false;
+    while(!win) {
+        Gameboard.printBoard();
+        Gameboard.playerAction();
+        win = Gameboard.winCheck();
+        Gameboard.switchPlayer();
+    }
 })();
