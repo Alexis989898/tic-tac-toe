be78891bc5cf5c6743b938cfc19ed1023f1f4cca
Alexis989898 <alexisvitor6@gmail.com>
2024-02-29 17:59:08 -0300

    JS remade with factory functions & reset button added

--- script.js
+++ script.js
@@ -1,77 +1,136 @@
-let Gameboard = {
-    board: Array(9).fill(""),
-    turnPlayer: "X",
-    updateBoard: function(e) {
+const Gameboard = (function() {
+
+    let board = Array(9).fill("");
+    let turnPlayer = "X";
+
+    const getBoard = function() {
+        return board;
+    }
+
+    const getTurnPlayer = function() {
+        return turnPlayer;
+    }
+
+    const setBoard = function(newBoard) {
+        board = newBoard;
+    };
+
+    const setTurnPlayer = function(newTurnPlayer) {
+        turnPlayer = newTurnPlayer;
+    };
+
+    const updateBoard = function(e, textDiv) {
         const clickedElement = e.target;
         const number = clickedElement.getAttribute("number");
-        if(this.board[number] == "X" || this.board[number] == "O") {
-            if(!(this.winCheck)) {
+
+        if (board[number] == "X" || board[number] == "O") {
+            if (!winCheck(textDiv)) {
                 textDiv.innerText = "Choose an available space";
             }
         } else {
-            clickedElement.innerText = this.turnPlayer;
-            this.board[number] = this.turnPlayer;
-            const win = this.winCheck();
-            if(!win) {this.switchPlayer();};
+            clickedElement.classList.add("checked");
+            clickedElement.innerText = turnPlayer;
+            board[number] = turnPlayer;
+            const win = winCheck(textDiv);
+            if(!win) { 
+                switchPlayer(textDiv); 
+            };
         }
 
-        console.log(this.board);
-    },
-    switchPlayer: function() {
-        if(this.turnPlayer == "X") {
-            this.turnPlayer = "O";
+        const boardSpaces = document.querySelectorAll(".boardspace");
+        boardSpaces.forEach(space => space.setAttribute("data-player", turnPlayer));
+    };
+
+    const switchPlayer = function (textDiv) {
+        if (turnPlayer == "X") {
+            turnPlayer = "O";
         } else {
-            this.turnPlayer = "X";
+            turnPlayer = "X";
         }
-        textDiv.innerText = Gameboard.turnPlayer + "'s turn";
-    },
-    winCheck: function() {
+        textDiv.innerText = turnPlayer + "'s turn";
+    };
+
+    const winCheck = function (textDiv) {
         let win = false;
-        for(let i = 0; i < 3; i++) {
-            if(
-                (this.board[i * 3] == this.turnPlayer &&
-                this.board[i * 3 + 1] == this.turnPlayer &&
-                this.board[i * 3 + 2] == this.turnPlayer) 
-                    ||
-                (this.board[i] == this.turnPlayer &&
-                this.board[i + 3] == this.turnPlayer &&
-                this.board[i + 6] == this.turnPlayer)
+        for (let i = 0; i < 3; i++) {
+            if (
+                (board[i * 3] == turnPlayer &&
+                    board[i * 3 + 1] == turnPlayer &&
+                    board[i * 3 + 2] == turnPlayer)
+                ||
+                (board[i] == turnPlayer &&
+                    board[i + 3] == turnPlayer &&
+                    board[i + 6] == turnPlayer)
             ) {
-                textDiv.innerText = this.turnPlayer + " won!";
+                textDiv.innerText = turnPlayer + " won!";
                 win = true;
                 break;
             }
         }
-        if( 
-            (this.board[0] == this.turnPlayer &&
-            this.board[4] == this.turnPlayer &&
-            this.board[8] == this.turnPlayer)
-                ||
-            (this.board[2] == this.turnPlayer &&
-                this.board[4] == this.turnPlayer &&
-                this.board[6] == this.turnPlayer)
-            )  {
-                textDiv.innerText = this.turnPlayer + " won!";
-                win = true;
-            }
-        if(!Gameboard.board.includes("") && !win) {
+        if (
+            (board[0] == turnPlayer &&
+                board[4] == turnPlayer &&
+                board[8] == turnPlayer)
+            ||
+            (board[2] == turnPlayer &&
+                board[4] == turnPlayer &&
+                board[6] == turnPlayer)
+        ) {
+            textDiv.innerText = turnPlayer + " won!";
+            win = true;
+        }
+        if (!board.includes("") && !win) {
             textDiv.innerText = "Draw";
             win = true;
         }
         return win;
+    };
+
+    return {
+        getBoard: getBoard,
+        getTurnPlayer: getTurnPlayer,
+        updateBoard: updateBoard,
+        switchPlayer: switchPlayer,
+        winCheck: winCheck,
+        setBoard: setBoard,
+        setTurnPlayer: setTurnPlayer
+    };
+})();
+
+const display = (function() {
+    // Getting divs
+    const divBoard = document.querySelector("#board");
+    const textDiv = document.querySelector("#text-div");
+    const resetButton = document.querySelector(".reset-btn");
+
+    // Reset button logic
+    resetButton.addEventListener("click", function() {
+        Gameboard.setBoard(Array(9).fill(""));
+        Gameboard.setTurnPlayer("X");
+        textDiv.innerText = Gameboard.getTurnPlayer() + "'s turn";
+
+        const boardSpaces = document.querySelectorAll(".boardspace");
+
+        boardSpaces.forEach(space => {
+            space.classList.remove("checked");
+            space.innerText = "";
+        });
+
+        boardSpaces.forEach(space => {
+            space.setAttribute("data-player", Gameboard.getTurnPlayer());
+        });
+    });
+
+    textDiv.innerText = Gameboard.getTurnPlayer() + "'s turn";
+
+    // Creating board
+    for (let i = 0; i < 9; i++) {
+        const boardElement = document.createElement("div");
+        boardElement.className = "boardspace";
+        boardElement.setAttribute("number", i);
+        boardElement.setAttribute("data-player", Gameboard.getTurnPlayer());
+        boardElement.addEventListener("click", (e) => Gameboard.updateBoard(e, textDiv));
+        boardElement.innerText = Gameboard.getBoard()[i];
+        divBoard.appendChild(boardElement);
     }
-};
-
-const divBoard = document.getElementById("board");
-const textDiv = document.getElementById("text-div");
-
-textDiv.innerText = Gameboard.turnPlayer + "'s turn";
-
-for(let i = 0; i < Gameboard.board.length; i++) {
-    const boardElement = document.createElement("div");
-    boardElement.className = "boardspace";
-    boardElement.setAttribute("number", i);
-    boardElement.addEventListener("click", () => Gameboard.updateBoard(event));
-    boardElement.innerText = Gameboard.board[i];
-    divBoard.appendChild(boardElement);
-}
+})();
